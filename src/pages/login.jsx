3810b0d205b96bcb4791247a4193e6149e6a9439
Matheus Porto <matheus.pereira11@gmail.com>
2025-05-26 18:43:12 -0300

    refactor(login): change css to styled components

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,79 +1,91 @@
 import React, { useState } from 'react';
-import styles from '../styles/login.module.css';
+import {
+  Base,
+  Hero,
+  HeroContent,
+  LoginForm,
+  RegisterFormBtn,
+  RegisterFormBtn2,
+  RegisterFormBtn4,
+  CliqueAqui,
+  PopupOverlay,
+  Popup
+} from '../styles/login';
 
 function Login() {
-    const [showPopup, setShowPopup] = useState(false);
-    const [email, setEmail] = useState('');
+  const [showPopup, setShowPopup] = useState(false);
+  const [email, setEmail] = useState('');
 
-    const handleEsqueciSenha = () => {
-        setShowPopup(true);
-    };
+  const handleEsqueciSenha = () => setShowPopup(true);
+  const handleClosePopup = () => setShowPopup(false);
+  const handleEmailChange = (e) => setEmail(e.target.value);
 
-    const handleClosePopup = () => {
-        setShowPopup(false);
-    };
+  const handleSubmitEmail = (e) => {
+    e.preventDefault();
+    console.log('E-mail enviado:', email);
+    setShowPopup(false);
+  };
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
+  return (
+    <Base>
+      <div className="container-fluid h-100 w-100 min-vh-100">
+        <div className="row w-100 h-100 min-vh-100">
+          <div className="col-md-6 p-0">
+            <Hero>
+              <HeroContent>
+                <h1 className="display-3">AHorta</h1>
+                <p className="lead">A maior comunidade orgânica do Brasil</p>
+              </HeroContent>
+            </Hero>
+          </div>
 
-    const handleSubmitEmail = (e) => {
-        e.preventDefault();
-        // Aqui você pode implementar a lógica para enviar o e-mail
-        console.log('E-mail enviado:', email);
-        setShowPopup(false);
-    };
+          <div className="col-md-1"></div>
 
-    return (
-        <div className={styles.base}>
-            <div className="container-fluid h-100 w-100 min-vh-100">
-                <div className="row w-100 h-100 min-vh-100">
-                    <div className={`${styles.hero} col-md-6 min-vh-100`}>
-                        <div className={styles.heroContent}>
-                            <h1 className="display-3">AHorta</h1>
-                            <p className="lead">A maior comunidade orgânica do Brasil</p>
-                        </div>
-                    </div>
-                    <div className="col-md-1"></div>
-                    <div className="col-md-4 row">
-                        <div className={`${styles.loginForm} w-100 p-5 min-vh-100 col-md-5`}>
-                            <h3 className="mb-4">Login</h3>
-                            <form>
-                                <div className="mb-4">
-                                    <label for="email" className="form-label">E-mail</label>
-                                    <input type="email" className="form-control" id="email" placeholder="Digite aqui o seu e-mail" required />
-                                </div>
-                                <div className="mb-4">
-                                    <label for="password" className="form-label">Senha</label>
-                                    <input type="password" className="form-control" id="password" placeholder="Insira aqui a sua senha" required />
-                                </div>
-                                <button type="submit" className={`btn w-100 mb-4 ${styles.registerFormBtn}`}>Entrar</button>
-                                <button type="button" className={`btn w-100 mb-4 ${styles.registerFormBtn4}`} onClick={handleEsqueciSenha}>Esqueci minha senha</button>
-                                <p className="text-center mt-4">Não tem cadastro ainda? <strong><a className={styles.cliqueAqui} href="register">Clique aqui</a></strong> e faça parte dessa comunidade orgânica!</p>
-                            </form>
-                        </div>
-                    </div>
-                    <div className="col-md-1"></div>
+          <div className="col-md-4 row">
+            <LoginForm className="w-100 p-5 min-vh-100 col-md-5">
+              <h3 className="mb-4">Login</h3>
+              <form>
+                <div className="mb-4">
+                  <label htmlFor="email" className="form-label">E-mail</label>
+                  <input type="email" className="form-control" id="email" placeholder="Digite aqui o seu e-mail" required />
                 </div>
-            </div>
 
-            {showPopup && (
-                <div className={styles.popupOverlay}>
-                    <div className={styles.popup}>
-                        <h3 className="mb-4">Recuperar Senha</h3>
-                        <form onSubmit={handleSubmitEmail}>
-                            <div className="mb-4">
-                                <label for="emailRecuperacao" className="form-label">E-mail</label>
-                                <input type="email" className="form-control" id="emailRecuperacao" value={email} onChange={handleEmailChange} placeholder="Digite aqui o seu e-mail" required />
-                            </div>
-                            <button type="submit" className={`btn w-100 ${styles.registerFormBtn}`}>Enviar</button>
-                            <button type="button" className={`btn mt-2 w-100 ${styles.registerFormBtn2}`} onClick={handleClosePopup}>Cancelar</button>
-                        </form>
-                    </div>
+                <div className="mb-4">
+                  <label htmlFor="password" className="form-label">Senha</label>
+                  <input type="password" className="form-control" id="password" placeholder="Insira aqui a sua senha" required />
                 </div>
-            )}
+
+                <RegisterFormBtn type="submit" className="mb-4">Entrar</RegisterFormBtn>
+                <RegisterFormBtn4 type="button" className="mb-4" onClick={handleEsqueciSenha}>Esqueci minha senha</RegisterFormBtn4>
+
+                <p className="text-center mt-4">
+                  Não tem cadastro ainda? <strong><CliqueAqui href="/register">Clique aqui</CliqueAqui></strong> e faça parte dessa comunidade orgânica!
+                </p>
+              </form>
+            </LoginForm>
+          </div>
+
+          <div className="col-md-1"></div>
         </div>
-    );
+      </div>
+
+      {showPopup && (
+        <PopupOverlay>
+          <Popup>
+            <h3 className="mb-4">Recuperar Senha</h3>
+            <form onSubmit={handleSubmitEmail}>
+              <div className="mb-4">
+                <label htmlFor="emailRecuperacao" className="form-label">E-mail</label>
+                <input type="email" className="form-control" id="emailRecuperacao" value={email} onChange={handleEmailChange} placeholder="Digite aqui o seu e-mail" required />
+              </div>
+              <RegisterFormBtn type="submit">Enviar</RegisterFormBtn>
+              <RegisterFormBtn2 type="button" className="mt-2" onClick={handleClosePopup}>Cancelar</RegisterFormBtn2>
+            </form>
+          </Popup>
+        </PopupOverlay>
+      )}
+    </Base>
+  );
 }
 
 export default Login;
